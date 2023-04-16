9c8103e5bfec754991e40d4b80ce97e3919220c9
[jo] <jonespjunior@gmail.com>
2023-04-16 15:16:56 -0400

    adicionei cd, tirei head

--- prototerminal.c
+++ prototerminal.c
@@ -15,21 +15,12 @@ char* pwd() {
     }
 }
 
-void print_first_lines(char *arquivo){
-    FILE *file = fopen(arquivo, "r");
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return;
+int cd(char* caminho) {
+    if (chdir(caminho) == -1) {
+        perror("Erro ao mudar o diretório");
+        return EXIT_FAILURE;
     }
-
-    char line[100];
-    int count = 0;
-    while (fgets(line, 100, file) && count < 10) {
-        printf("%s", line);
-        count=count+1;
-    }
-
-    fclose(file);
+    return EXIT_SUCCESS;
 }
 
 int ls(void ){
@@ -40,7 +31,7 @@ int ls(void ){
     strcpy(caminho, pwd());
     dirp = opendir(caminho);
     if( dirp == NULL ) {
-        perror( "nao foi possivel abrir diretorio");
+        perror( "Nao foi possivel localizar" );
     } else {
         for(;;) {
             direntp = readdir( dirp );
@@ -55,42 +46,27 @@ int ls(void ){
     return EXIT_SUCCESS;
 }
 
-int contemDuasPalavras(char *texto) {
-    char *espaco = strchr(texto, ' '); // Procura o primeiro espaço em branco na string
-    if (espaco != NULL) { // Se encontrou um espaço em branco
-        char *segundaPalavra = espaco + 1; // Pega o endereço da segunda palavra
-        if (strchr(segundaPalavra, ' ') == NULL) { // Verifica se a segunda palavra não contém outro espaço
-            return 1; // Se não contém, há duas palavras
-        }
-    }
-    return 0; // Caso contrário, há uma ou mais de duas palavras
-}
-
-
-
 int main() {
     char comando[100];
     bool terminal = true;
     printf("Use o comando 'exit' para sair do terminal...\n");
     while (terminal) {
         scanf("%s", comando);
-        if(contemDuasPalavras==1){
-            if (strcmp(comando, "pwd") == 0) {
-                printf("%s\n",pwd());
-            }else if(strcmp(comando, "ls") == 0) {
-                ls();
-            }else if (strcmp(comando, "exit") == 0) {
-                printf("Saindo...\n");
-                terminal = false;
-            }else {
-                printf("Comando invalido\n");
-            }
-        }
-        else{
-
+        if (strcmp(comando, "pwd") == 0) {
+            printf("%s\n",pwd());
+        } else if(strcmp(comando, "ls") == 0) {
+			ls();
+	    } else if (strcmp(comando, "cd") == 0) {
+            char caminho[1024];
+            scanf("%s", caminho);
+            cd(caminho);
+        } else if (strcmp(comando, "exit") == 0) {
+            printf("Saindo...\n");
+            terminal = false;
+        } else {
+            printf("Comando invalido\n");
         }
     }
-    //print_first_lines(filename)
     
     return 0;
 }
