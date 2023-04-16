e63f24040cc26552e320c1c0bdc33eb4c77b3b34
[jo] <jonespjunior@gmail.com>
2023-04-15 21:43:35 -0400

    adicionei função head (falta ajeitar no main)

--- prototerminal.c
+++ prototerminal.c
@@ -15,6 +15,23 @@ char* pwd() {
     }
 }
 
+void print_first_lines(char *arquivo){
+    FILE *file = fopen(arquivo, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    char line[100];
+    int count = 0;
+    while (fgets(line, 100, file) && count < 10) {
+        printf("%s", line);
+        count=count+1;
+    }
+
+    fclose(file);
+}
+
 int ls(void ){
     DIR* dirp;
     struct dirent* direntp;
@@ -38,24 +55,42 @@ int ls(void ){
     return EXIT_SUCCESS;
 }
 
+int contemDuasPalavras(char *texto) {
+    char *espaco = strchr(texto, ' '); // Procura o primeiro espaço em branco na string
+    if (espaco != NULL) { // Se encontrou um espaço em branco
+        char *segundaPalavra = espaco + 1; // Pega o endereço da segunda palavra
+        if (strchr(segundaPalavra, ' ') == NULL) { // Verifica se a segunda palavra não contém outro espaço
+            return 1; // Se não contém, há duas palavras
+        }
+    }
+    return 0; // Caso contrário, há uma ou mais de duas palavras
+}
+
+
+
 int main() {
     char comando[100];
     bool terminal = true;
     printf("Use o comando 'exit' para sair do terminal...\n");
     while (terminal) {
         scanf("%s", comando);
-        if (strcmp(comando, "pwd") == 0) {
-            printf("%s\n",pwd());
-        }else if(strcmp(comando, "ls") == 0) {
-            //printf("O comando ls está sendo executado\n");
-			ls();
-	    }else if (strcmp(comando, "exit") == 0) {
-            printf("Saindo...\n");
-            terminal = false;
-        }else {
-            printf("Comando invalido\n");
+        if(contemDuasPalavras==1){
+            if (strcmp(comando, "pwd") == 0) {
+                printf("%s\n",pwd());
+            }else if(strcmp(comando, "ls") == 0) {
+                ls();
+            }else if (strcmp(comando, "exit") == 0) {
+                printf("Saindo...\n");
+                terminal = false;
+            }else {
+                printf("Comando invalido\n");
+            }
+        }
+        else{
+
         }
     }
+    //print_first_lines(filename)
     
     return 0;
 }
