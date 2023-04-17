56f3f24505b8824778fa1fba01c9a1cd9b585316
sarahjulia1909 <sarah.santos@icomp.ufam.edu.br>
2023-04-17 11:52:22 -0400

    adição de função que verifica tipo de comando versão simples

--- prototerminal.c
+++ prototerminal.c
@@ -63,31 +63,85 @@ int ls(void ){
     return EXIT_SUCCESS;
 }
 
-int main() {
-    char comando[100];
-    bool terminal = true;
-    printf("Use o comando 'exit' para sair do terminal...\n");
-    while (terminal) {
-        scanf("%s", comando);
-        if (strcmp(comando, "pwd") == 0) {
-            printf("%s\n",pwd());
-        } else if(strcmp(comando, "ls") == 0) {
-			ls();
-	    } else if (strcmp(comando, "cd") == 0) {
-            char caminho[1024];
-            scanf("%s", caminho);
-            cd(caminho);
-        } else if (strcmp(comando, "head") == 0) {
-            char arquivo[1024];
-            scanf("%s", arquivo);
-            head(arquivo);
-        }else if (strcmp(comando, "exit") == 0) {
-            printf("Saindo...\n");
-            terminal = false;
-        } else {
-            printf("Comando invalido\n");
+int verifica_comando(char* comando){
+
+	if (strstr(comando, "./") != NULL) {
+            return 1;
+        } else if (strstr(comando, "|") != NULL) {
+            return 2; 
+        } else if (strstr(comando, "&") != NULL) {
+            return 3; 
+        } else if (strstr(comando, ">") != NULL) {
+            return 4; 
+        } else if (strstr(comando, "<") != NULL) {
+            return 5; 
         }
-    }
+      
     
+
     return 0;
+
+}
+
+
+int main() {
+  char comando[100];
+  bool terminal = true;
+  printf("Use o comando 'exit' para sair do terminal...\n");
+
+  while (terminal) {
+
+     fgets(comando, 100, stdin);
+     comando[strcspn(comando, "\n")] = '\0';
+
+	int tipo_comando = verifica_comando(comando);
+
+	//printf("tipooo: %d",tipo_comando);
+
+	switch(tipo_comando){
+		case 0:
+			if (strcmp(comando, "pwd") == 0) {
+      			printf("%s\n", pwd());
+    		} else if (strcmp(comando, "ls") == 0) {
+      			ls();
+    		} else if (strstr(comando, "cd") != NULL) {
+      			char caminho[1024];
+      			scanf("%s", caminho);
+      			cd(caminho);
+			} else if (strcmp(comando, "head") == 0) {
+            	char arquivo[1024];
+            	scanf("%s", arquivo);
+            	head(arquivo);
+        	} else if (strcmp(comando, "exit") == 0) {
+      			printf("Saindo...\n");
+      			terminal = false;
+    		}else{
+				printf("Comando inválido\n");
+			}
+		 		break;
+            case 1: 
+                printf("executar comando ./....\n");
+                break;
+            case 2: 
+                printf("executar comando |....\n");
+                break;
+            case 3: 
+                printf("executar comando &....\n");
+                break;
+            case 4: 
+                printf("executar comando > saída ....\n");
+                break;
+            case 5: 
+                printf("executar comando < entrada....\n");
+                break;
+            default:
+                printf("Comando invalido\n");
+                break;	
+
+		
+	}
+
+}
+     
+  return 0;
 }
